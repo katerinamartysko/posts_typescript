2f9ca5fe41536e00f51556b3353b69521e50b793
Katerina <katerinamartysko@gmail.com>
2022-10-11 12:04:51 +0300

    add total count

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import { useFetching } from '../hooks/useFetching';
-import PostService from '../api/PostService';
 import { useDispatch } from 'react-redux';
+import PostService from '../api/PostService';
+import { useFetching } from '../hooks/useFetching';
 import { getPosts } from '../store/posts/actions';
+import { getPageCount } from '../utils/pages';
 
 const Posts = () => {
   const dispatch = useDispatch();
   const [limit] = useState(10);
+  const [, setTotalPages] = useState<number>(0);
 
   const [fetchPosts] = useFetching(async (limit: number, page: number) => {
     const response = await PostService.getAll(limit, page);
     dispatch(getPosts(response.data));
+    const totalCount = response.headers['x-total-count'];
+    setTotalPages(getPageCount(Number(totalCount), limit));
   });
 
   useEffect(() => {
