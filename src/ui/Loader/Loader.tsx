e0459a5c4b0ed12e9c679284b471de0b7e6d9b3b
Katerina <katerinamartysko@gmail.com>
2022-10-11 16:21:33 +0300

    add error and loader

---
+++ src/ui/Loader/Loader.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// @ts-ignore
+import cl from './Loader.module.css'
+
+const Loader = () => {
+  return (
+    <div className={cl.loader} />
+  );
+};
+
+export default Loader;
