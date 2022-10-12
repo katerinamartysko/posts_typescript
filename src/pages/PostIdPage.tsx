3b733caa89f863b53d6ddf7b6eecece72b624ad0
Katerina <katerinamartysko@gmail.com>
2022-10-12 17:55:39 +0300

    add page PostId

---
+++ src/pages/PostIdPage.tsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useFetching } from '../hooks/useFetching';
+import PostService from '../api/PostService';
+import { Comments, Post } from '../api/types';
+import Loader from '../components/ui/Loader/Loader';
+
+const PostIdPage = () => {
+  const params = useParams();
+  const [post, setPost] = useState<Post | null>(null);
+  const [comments, setComments] = useState<Array<Comments>>([]);
+
+  const [fetchPostById, isLoading] = useFetching(async (id: number) => {
+    const response = await PostService.getById(id);
+    setPost(response.data);
+  });
+
+  const [fetchComments, isCommentsLoading] = useFetching(async (id: number) => {
+    const response1 = await PostService.getCommentsByPostId(id);
+    setComments(response1.data);
+  });
+
+  useEffect(() => {
+    fetchPostById(params.id);
+    fetchComments(params.id);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (!post) return <Loader />;
+  return (
+    <div>
+      <h1>ВЫ ОТКРЫЛИ СТРАНИЦУ С ПОСТА ID {params.id}</h1>
+      {isLoading
+        ? <Loader />
+        : <div>{post.id}. {post.title}
+          <div>{post.body}
+          </div>
+        </div>
+      }
+      <h1>Коментарии</h1>
+      {isCommentsLoading
+        ? <Loader />
+        : <div> {comments.map(comment =>
+          <div key={comment.id} style={{ marginTop: 16 }}>
+            <h5>{comment.email}</h5>
+            <div>{comment.body}</div>
+          </div>
+        )}
+        </div>
+      }
+    </div>
+  );
+};
+
+export default PostIdPage;
