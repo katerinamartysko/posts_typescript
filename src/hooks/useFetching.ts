dc47dc49b4980fcbddda5df403909d623ec208be
Katerina <katerinamartysko@gmail.com>
2022-10-12 17:52:01 +0300

    add logic for open post

--- src/hooks/useFetching.ts
+++ src/hooks/useFetching.ts
@@ -1,15 +1,15 @@
 import { useState } from 'react';
 
-type UseFetchingCallback = (limit: number, page: number) => Promise<void>
+type UseFetchingCallback = (...arg: Array<any>) => Promise<void>
 
 export const useFetching = (callback: UseFetchingCallback): [UseFetchingCallback, boolean, (string | null)] => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
 
-  const fetching = async (limit: number, page: number): Promise<void> => {
+  const fetching = async (...args: Array<unknown>): Promise<void> => {
     try {
       setIsLoading(true);
-      await callback(limit, page);
+      await callback(...args);
       setError(null);
     } catch (e: unknown | Error) {
       setError((e as Error).message);
