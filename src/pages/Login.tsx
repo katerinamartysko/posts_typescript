a37332a5e528b3d653636f0feba31baa7c2eb8a5
Katerina <katerinamartysko@gmail.com>
2022-10-27 15:14:49 +0300

    add formik

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,11 @@
-import React, { FormEvent, useContext } from 'react';
-import { AuthContext } from '../contex';
-import { Button, TextField } from '@mui/material';
+import React, { FC, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Alert, Button, TextField } from '@mui/material';
 import { makeStyles } from 'tss-react/mui';
+import { Form, Formik } from 'formik';
+import * as yup from 'yup';
 import { AppTheme } from '../utils/them';
+import { AuthContext } from '../contex';
 import '../App.css';
 
 const useStyles = makeStyles()((theme: AppTheme) => ({
@@ -19,16 +21,30 @@ const useStyles = makeStyles()((theme: AppTheme) => ({
   myInp: {
     marginLeft: theme.spacing(1),
     width: '70%',
-    padding: theme.spacing(0.5, 1),
   },
 }));
-const Login = () => {
+
+interface LoginFormValue {
+  login: string;
+  password: string;
+}
+
+const initialValue: LoginFormValue = {
+  login: '',
+  password: '',
+};
+
+const validationsSchema = yup.object().shape({
+  login: yup.string().typeError('Должно быть строкой').required('Обязательно'),
+  password: yup.string().typeError('Должно быть строкой').required('Обязательно'),
+});
+
+const Login: FC = () => {
   const { setIsAuth } = useContext(AuthContext);
   const navigate = useNavigate();
   const { classes } = useStyles();
 
-  const login = (event: FormEvent) => {
-    event.preventDefault();
+  const login = (values: LoginFormValue): void => {
     setIsAuth(1);
     localStorage.setItem('auth', '1');
     navigate('/posts');
@@ -37,25 +53,48 @@ const Login = () => {
   return (
     <div>
       <h1>Страница для логина</h1>
-      <form className={classes.root} onSubmit={login}>
-        <TextField
-          id="outlined-basic"
-          label="      Введите логин"
-          variant="outlined"
-          className={classes.myInp}
-          type="text"
-        />
-        <TextField
-          id="outlined-basic"
-          label="      Введите пароль"
-          variant="outlined"
-          className={classes.myInp}
-          type="text"
-        />
-        <Button className={classes.login} variant="outlined" type="submit">
-          ВОЙТИ
-        </Button>
-      </form>
+      <div>
+        <Formik initialValues={initialValue} validateOnBlur onSubmit={login} validationSchema={validationsSchema}>
+          {({ values, errors, touched, handleChange, handleBlur, isValid, handleSubmit, dirty }) => (
+            <Form>
+              <div>
+                <TextField
+                  id="outlined-basic"
+                  label="Введите логин"
+                  variant="outlined"
+                  className={classes.myInp}
+                  type="text"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.login}
+                  name="login"
+                />
+              </div>
+              {touched.login && errors.login && <Alert severity="error">{errors.login}</Alert>}
+              <div>
+                <TextField
+                  id="outlined-basic"
+                  label="Введите пароль"
+                  variant="outlined"
+                  className={classes.myInp}
+                  type="text"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.password}
+                  name="password"
+                  error={touched.password && Boolean(errors.password)}
+                  helperText={touched.password && errors.password && errors.password}
+                />
+              </div>
+              {touched.password && errors.password && <Alert severity="error">{errors.password}</Alert>}
+
+              <Button disabled={!isValid && !dirty} type="submit" variant="outlined" className={classes.login}>
+                ВОЙТИ
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 };
